2304af77ef643019e1df70efe448b787140de3d9
Ryan Cox <ryanedwincox@gmail.com>
2015-02-19 22:46:09 -0800

    added delay to insure frames are recorded at the correct fps

--- cam.py
+++ cam.py
@@ -13,6 +13,7 @@ python cam.py 1 underwaterCam
 """
 
 from time import localtime, strftime
+import time
 import numpy as np
 import cv2
 import sys
@@ -32,21 +33,33 @@ elif len(sys.argv) == 3:
 # Create video capture object
 cap = cv2.VideoCapture(cameraNum)
 
-# Get video properties
-fmwd = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-fmht = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
-fps = int(10)
-#print "FPS: " + str(cap.get(cv2.cv.CV_CAP_PROP_FPS))
-
 # Define the codec and create VideoWriter object
 fourcc = cv2.cv.CV_FOURCC('M','J','P','G')
+wd = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+ht = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+fps = int(14)
 outputFileName = basename + str(cameraNum) + '_' + strftime('%Y%m%d_%H %M %S', localtime())
-out = cv2.VideoWriter(outputFileName + '.avi',fourcc, fps, (fmwd,fmht), True)
+out = cv2.VideoWriter(outputFileName + '.avi',fourcc, fps, (wd,ht), True)
+
+# Start time
+t = time.clock() # starts at zero when the program starts
 
 while(cap.isOpened()):
-    
     # Capture frame-by-frame
     ret, frame = cap.read()
+    
+    # Finish waiting for 40ms before continuing to record and display frame
+    d=0
+    # print str(time.clock() - t)
+    while (time.clock() - t < 0.0714285):
+        d=d+1
+    print str(d)
+    t = time.clock()
+    print (t)
+    
+    # # Uncomment to display time between frames
+    # print str(time.clock() - t)
+    # t = time.clock()
     
     # Write frame to file
     out.write(frame)
